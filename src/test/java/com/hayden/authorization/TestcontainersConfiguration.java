dbc0c33a088fd86233d26f89658ec317b6691b23
hayden.rear <hayden.rear@gmail.com>
2024-07-12 10:39:01 -0400

    updated

---
+++ src/test/java/com/hayden/authorization/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.hayden.authorization;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
