923183104ae49dbee89dc983706c5b38e0dabc3f
hayden.rear <hayden.rear@gmail.com>
2024-09-02 20:50:08 -0400

    updating prototype for rest calls to model server

--- src/main/java/com/hayden/authorization/AuthorizationApplication.java
+++ src/main/java/com/hayden/authorization/AuthorizationApplication.java
@@ -8,7 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jdbc.JdbcRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @Import({HibernateConfig.class, JpaConfig.class, QueryDslConfig.class})
@@ -17,7 +25,5 @@ public class AuthorizationApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthorizationApplication.class, args);
     }
-
-
 
 }
