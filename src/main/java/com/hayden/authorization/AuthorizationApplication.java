17e3cc537048759c0b78a8cd7978a008da956c80
hayden.rear <hayden.rear@gmail.com>
2025-02-12 14:48:45 -0500

    updated

--- src/main/java/com/hayden/authorization/AuthorizationApplication.java
+++ src/main/java/com/hayden/authorization/AuthorizationApplication.java
@@ -5,18 +5,7 @@ import com.hayden.persistence.config.JpaConfig;
 import com.hayden.persistence.config.QueryDslConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jdbc.JdbcRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.client.ClientHttpRequestExecution;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.web.client.RestTemplate;
-
-import java.io.IOException;
 
 @SpringBootApplication
 @Import({HibernateConfig.class, JpaConfig.class, QueryDslConfig.class})
