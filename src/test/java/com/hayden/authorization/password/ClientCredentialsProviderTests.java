3127c8ed61d22862d2705b330b9df5fc4acbf5ea
hayden.rear <hayden.rear@gmail.com>
2025-10-13 15:01:14 -0400

    updated to use key

---
+++ src/test/java/com/hayden/authorization/password/ClientCredentialsProviderTests.java
@@ -0,0 +1,94 @@
+package com.hayden.authorization.password;
+
+import com.unboundid.util.Base64;
+import lombok.SneakyThrows;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Stream;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+@ActiveProfiles("test-auth")
+public class ClientCredentialsProviderTests {
+
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    JwtDecoder jwtDecoder;
+    @Autowired
+    ClientRegistrationRepository clientRegistrationRepository;
+
+    @SneakyThrows
+    @Test
+    public void doClientCredentialsProviderTests() {
+        AtomicReference<String> s = new AtomicReference<>();
+        mockMvc.perform(
+                        post("/oauth2/token")
+                                .with(csrf())
+                                .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                                .param("grant_type", AuthorizationGrantType.CLIENT_CREDENTIALS.getValue())
+                                .param("client_id", "cdc-oauth2-client")
+                                .param("client_secret", "234234lkjsldkdjfsd")
+                )
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.access_token", Matchers.notNullValue()))
+                .andExpect(jsonPath("$.access_token", Matchers.is(new BaseMatcher<String>() {
+                    @Override
+                    public boolean matches(Object o) {
+                        var decoded = Assertions.assertDoesNotThrow(() -> jwtDecoder.decode(o.toString()));
+                        s.set(o.toString());
+                        return decoded != null && decoded instanceof Jwt j;
+                    }
+
+                    @Override
+                    public void describeMismatch(Object o, Description description) {
+                        description.appendText("%s not contain scopes!".formatted(o.toString()));
+                    }
+
+                    @Override
+                    public void describeTo(Description description) {
+                        description.appendText("Did not contain scopes!");
+                    }
+                })))
+                .andDo(print());
+
+        mockMvc.perform(
+                       get("/api/v1/credits/get-credits")
+                               .with(csrf())
+                               .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                               .param("grant_type", AuthorizationGrantType.JWT_BEARER.getValue())
+                               .param("client_id", "cdc-oauth2-client")
+                               .param("client_secret", "234234lkjsldkdjfsd")
+                               .header(HttpHeaders.AUTHORIZATION, "Bearer %s".formatted(s.get()))
+               )
+               .andExpect(status().is2xxSuccessful())
+               .andExpect(jsonPath("$", Matchers.notNullValue()))
+               .andDo(print());
+    }
+
+}
